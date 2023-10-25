e60fdbc6bf72cd9536dd9d899aa4828f6365dd36
NAlexH2 <alex@nah-red.net>
2023-10-24 21:39:07 -0700

    adding ticker

--- src/pageComponents/unsubForm.tsx
+++ src/pageComponents/unsubForm.tsx
@@ -4,15 +4,24 @@ export const UnsubForm = () => {
   return (
     <form action="">
       <div className="mt-4">
-        <p className="text-left">
-          <span className="text-red-500">*</span>Required
-        </p>
         <input
           className="pl-3 pr-3 py-3 rounded-lg text-xl outline-none text-black"
           placeholder="Email Address"
           type="email"
           name="email"
           required
+        />
+        <p className="text-left ml-32">
+          <span className="text-red-500">*</span>Required
+        </p>
+      </div>
+      <div className="flex justify-center mx-4 mt-5">
+        <input
+          className="w-auto min-w-[435px] rounded-lg p-3 outline-none
+            text-black"
+          name="stockTicker"
+          type="text"
+          placeholder="Enter stock ticker (APPL, NVDA, etc.)"
         />
       </div>
       <div className="mt-4">
