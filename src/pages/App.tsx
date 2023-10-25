dff7bbee2d4352734123f47e63cb73463d8bd9bc
Alex Harris <32123635+NAlexH2@users.noreply.github.com>
2023-10-25 12:15:09 -0700

    Rudimetary Webpage (SW1) (#11)
    * Slight progress.
    
    * Progress
    
    * react logo not used
    
    * adding framer motion
    
    * sub or unsub component created
    
    * subscribe form created
    
    * custom button component created
    
    * unsubscribe form
    
    * light animation for first load
    
    * refactor and cleanup
    
    * button additions
    
    * adding ticker
    
    * Correction usage. Fixing typos.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,22 +1,17 @@
-import React from "react";
-import logo from "../images/logo.svg";
 import "../styles/App.css";
-import HelloWorld from "../pageComponents/helloworld";
+import AboutApp from "../pageComponents/aboutApp";
+import SubOrUnsub from "src/pageComponents/subOrUnsub";
+import AnimateTx from "src/util/animateTx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="flex justify-center ">
-          <img src={logo} className="App-logo" alt="logo" />
-          <span className="Chart-Emoji">
-            <span className="text-[200px]">📈</span>
-            <div className="text-xs -mt-10">(hover over me!)</div>
-          </span>
+    <div className="container mx-auto">
+      <AnimateTx>
+        <div className="App">
+          <AboutApp />
+          <SubOrUnsub />
         </div>
-
-        <HelloWorld />
-      </header>
+      </AnimateTx>
     </div>
   );
 }
