3f165165878f16a9efababb3e5319eae5f6f3130
NAlexH2 <alex@nah-red.net>
2023-10-24 21:32:57 -0700

    subscribe form created

---
+++ src/pageComponents/subscribeForm.tsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import SWButton from "./swButton";
+
+const buy: string = "Buy";
+const sell: string = "Sell";
+let buyOrSell: string = "";
+
+export const SubscribeForm = () => {
+  const [activeOption, setCurrentOption] = useState("Reset");
+  const updateParentState = (option: string) => {
+    setCurrentOption(option);
+    buyOrSell = option;
+  };
+  const buttonStyleSelector = (toChange: string) => {
+    return activeOption === toChange ? "SW-Button-Active " : "SW-Button ";
+  };
+
+  return (
+    <>
+      <form action="">
+        <div className="flex justify-center mx-4 mt-5">
+          <input
+            className="mt-5 w-auto min-w-[435px] rounded-lg p-3 outline-none
+            text-black"
+            name="stockTicker"
+            type="text"
+            placeholder="Enter stock ticker (APPL, NVDA, etc.)"
+          />
+        </div>
+        <div className="flex justify-center mt-2">
+          <SWButton
+            onUpdate={updateParentState}
+            buttonText={buy}
+            className={
+              buttonStyleSelector(buy) +
+              `mx-4 w-auto min-w-[200px] h-10 transition ease-in-out
+              duration-300`
+            }
+            type="button"
+          />
+          <SWButton
+            onUpdate={updateParentState}
+            buttonText={sell}
+            className={
+              buttonStyleSelector(sell) +
+              `mx-4 w-auto min-w-[200px] h-10 transition ease-in-out 
+              duration-300`
+            }
+            type="button"
+          />
+        </div>
+        {activeOption !== "Reset" && (
+          <>
+            <div className="flex justify-center mx-4 mt-5">
+              <input
+                className="mt-5 w-auto min-w-[100px] rounded-lg p-3 outline-none
+              text-black"
+                name="stockTicker"
+                type="number"
+                min={0}
+                placeholder="Enter target price"
+              />
+            </div>
+            <div className="flex justify-center mt-8">
+              <div>
+                <input
+                  className="mr-2 pl-3 pr-3 py-3 rounded-lg text-xl outline-none
+                  text-black"
+                  placeholder="Name"
+                  type="text"
+                  name="name"
+                  required
+                />
+                <p className="text-left ml-4">
+                  <span className="text-left text-red-500">*</span>Required
+                </p>
+              </div>
+              <div>
+                <input
+                  className="ml-2 pl-3 pr-3 py-3 rounded-lg text-xl outline-none 
+                  text-black"
+                  placeholder="Email Address"
+                  type="email"
+                  name="email"
+                  required
+                />
+                <p className="text-left ml-4">
+                  <span className="text-left text-red-500">*</span>Required
+                </p>
+              </div>
+            </div>
+            <div className="flex"></div>
+            <div className="mt-4">
+              <SWButton
+                type="submit"
+                buttonText="Submit"
+                className="SW-Button mx-4 min-w-[150px]"
+              />
+            </div>
+          </>
+        )}
+      </form>
+    </>
+  );
+};
+
+export default SubscribeForm;
