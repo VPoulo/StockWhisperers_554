86482248d94a29ab67b66c6a67faddfb609a6242
NAlexH2 <alex@nah-red.net>
2023-10-24 21:34:31 -0700

    refactor and cleanup

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,28 +1,17 @@
-import React, { useState } from "react";
 import "../styles/App.css";
 import AboutApp from "../pageComponents/aboutApp";
-import SWButton from "src/pageComponents/swButton";
+import SubOrUnsub from "src/pageComponents/subOrUnsub";
+import AnimateTx from "src/util/animateTx";
 
 function App() {
-  // const [activeOption, setCurrentOption] = useState(false);
-
   return (
     <div className="container mx-auto">
-      <header className="App">
-        <AboutApp />
-        <div className="flex">
-          <SWButton
-            text="Buy"
-            className="SW-Button m-4 w-20 h-10 hover:text-emerald-700"
-            // explore contexts for passing in states and modifying them
-          />
-          <SWButton
-            text="Sell"
-            className="SW-Button m-4 w-20 h-10 hover:text-emerald-700"
-            // explore contexts for passing in states and modifying them
-          />
+      <AnimateTx>
+        <div className="App">
+          <AboutApp />
+          <SubOrUnsub />
         </div>
-      </header>
+      </AnimateTx>
     </div>
   );
 }
