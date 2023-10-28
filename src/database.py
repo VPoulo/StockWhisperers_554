56b9c68ab68749cf8e6d10dd14d05843fc0de841
sharonoba <56489515+sharonoba@users.noreply.github.com>
2023-10-27 23:51:27 -0700

    added temporary database

--- src/database.py
+++ src/database.py
@@ -3,9 +3,10 @@ import requests
 import pandas as pd
 
 API_KEY = "8FEghfdkqaH3lQ923eyGZewt5Upf7eqC"
-filename = "10262023"  # Currently records date
+stockFile = "stocks"  # Currently holds stock data
+userFile = "user.csv"  # Currently holds user data
 
-def getStockDaily(API_KEY, filename):
+def getStockDaily(API_KEY, stockFile):
 
     url = f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09?adjusted=true&apiKey={API_KEY}"
     # Make a GET request to the API and fetch the data
@@ -14,13 +15,16 @@ def getStockDaily(API_KEY, filename):
     if response.status_code == 200:
         data = response.json()
         df = pd.DataFrame(data['results'])
-        df.to_csv(f"{filename}_stock_data.csv", index=False)
-        print(f"Data saved successfully to {filename}_stock_data.csv")
+        df.to_csv(f"{stockFile}_stock_data.csv", index=False)
+        print(f"Data saved successfully to {stockFile}.csv")
     else:
         print(f"Request failed with status code {response.status_code}")
 
-def getAllUsers():
-    
+def getAllUsers(userFile):
+    user = pd.read_csv(userFile, delimiter=',')
+
+    print(user)
+
 if __name__ == "__main__":
 
     getStockDaily(API_KEY, filename)
