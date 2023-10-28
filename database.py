7805f987d58ba2c5bd832f6a569f100a2c5f0e15
sharonoba <56489515+sharonoba@users.noreply.github.com>
2023-10-28 13:08:09 -0700

    modified apikey and added json dump

--- database.py
+++ database.py
@@ -3,22 +3,46 @@
 # o:open, t:timestamp, v:volume, vw:vol weighted avg price
 import requests
 import pandas as pd
+import os
+import re
+import json
 
-__API_KEY = "8FEghfdkqaH3lQ923eyGZewt5Upf7eqC"
-__STOCK_FILE = "./database/stocks.csv"  # Currently holds stock data
+__BASH_PATH = os.path.expanduser('~/.bash_profile')
+__STOCK_JSON = "./database/stocks.json"  # Currently holds stock data
+__STOCK_CSV = "./database/stocks.csv"
 __USER_FILE = "./database/users.csv"  # Currently holds user data
 
-def getStockDaily():
+def extract_api_key(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+        match = re.search(r'^export\s+POLYGON_API_KEY=(.*)$', content, re.MULTILINE)
+        if match:
+            api_key = match.group(1)
+        else:
+            raise ValueError("POLYGON_API_KEY not found in .bash_profile or .bashrc")
+    
+    os.environ['POLYGON_API_KEY']=api_key
+    polygon_api_key = os.environ.get('POLYGON_API_KEY')
+    polygon_api_key = polygon_api_key.replace('"', '')
+    return polygon_api_key
 
-    url = f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09?adjusted=true&apiKey={__API_KEY}"
-    # Make a GET request to the API and fetch the data
-    response = requests.get(url)
+def getStockDaily():
+    
+    if __API_KEY is None:
+        raise EnvironmentError("Polygon API key not found.  Set the environment variable")
+
+    else:
+        url = f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09?adjusted=true&apiKey={__API_KEY}"
+        # Make a GET request to the API and fetch the data
+        response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
         df = pd.DataFrame(data['results'])
-        df.to_csv(__STOCK_FILE, index=False)
-        print(f"Data saved successfully to {__STOCK_FILE}")
+        df.to_csv(__STOCK_CSV, index=False)
+        with open(__STOCK_JSON, 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+            print(f"Data saved successfully to {__STOCK_JSON}")
     else:
         print(f"Request failed with status code {response.status_code}")
 
@@ -28,6 +52,6 @@ def getAllUsers():
     print(user)
 
 if __name__ == "__main__":
-
+    __API_KEY = extract_api_key(__BASH_PATH)
     getStockDaily()
     getAllUsers()
