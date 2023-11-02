a42941b4216c9d810bb67c434389a749e9d12a71
NAlexH2 <alex@nah-red.net>
2023-11-02 10:12:50 -0700

    adding onClick prop for form submit

--- src/pageComponents/swButton.tsx
+++ src/pageComponents/swButton.tsx
@@ -1,23 +1,31 @@
+import React from "react";
+
 interface ButtonProps {
   buttonText: string;
   onUpdate?: (data: string) => void;
   className?: string;
   id?: string;
   type?: "button" | "submit" | "reset" | undefined;
+  onClick?: any;
 }
 
 export const SWButton = ({
   buttonText,
   onUpdate,
+  onClick,
   className,
   id,
   type,
 }: ButtonProps) => {
-  const handleClick = () => {
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
     if (onUpdate) {
       onUpdate(buttonText);
     }
+    if (onClick) {
+      onClick(event);
+    }
   };
+
   return (
     <button className={className} id={id} onClick={handleClick} type={type}>
       <div className="-mt-1">{buttonText}</div>
