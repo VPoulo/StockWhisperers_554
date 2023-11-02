1c2ce3f3416669fe15c4ee6936900a059a9f843f
NAlexH2 <alex@nah-red.net>
2023-11-02 10:12:32 -0700

    wip for form submission to backend

--- src/pageComponents/subscribeForm.tsx
+++ src/pageComponents/subscribeForm.tsx
@@ -1,23 +1,64 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import SWButton from "./swButton";
 
 const buy: string = "Buy";
 const sell: string = "Sell";
-let buyOrSell: string = "";
 
-export const SubscribeForm = () => {
-  const [activeOption, setCurrentOption] = useState("Reset");
+function SubscribeForm() {
+  const formName = "name";
+  const formEmail = "email";
+  const formTicker = "ticker";
+  const formTargetPrice = "targetPrice";
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [ticker, setTicker] = useState("");
+  const [buyOrSell, setBuyOrSell] = useState("Reset");
+  const [targetPrice, setTargetPrice] = useState(0);
+
   const updateParentState = (option: string) => {
-    setCurrentOption(option);
-    buyOrSell = option;
+    setBuyOrSell(option);
   };
   const buttonStyleSelector = (toChange: string) => {
-    return activeOption === toChange ? "SW-Button-Active " : "SW-Button ";
+    return buyOrSell === toChange ? "SW-Button-Active " : "SW-Button ";
+  };
+
+  const handleInputUpdate = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    inputID: string
+  ) => {
+    const updateValue = e.target.value;
+    if (inputID === formName) {
+      setName(updateValue);
+      console.log("Name: " + updateValue);
+    } else if (inputID === formEmail) {
+      setEmail(updateValue);
+      console.log("Email: " + updateValue);
+    } else if (inputID === formTicker) {
+      setTicker(updateValue);
+      console.log("Ticker: " + updateValue);
+    } else if (inputID === formTargetPrice) {
+      setTargetPrice(parseInt(updateValue, 10));
+      console.log("Target price: " + updateValue + " | " + targetPrice);
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
+  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("button clicked");
+    const formEvent = new Event("submit", {
+      bubbles: true,
+    }) as unknown as React.FormEvent<HTMLFormElement>;
+    handleSubmit(formEvent);
   };
 
   return (
     <>
-      <form action="">
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
         <div className="flex justify-center mt-8">
           <div>
             <input
@@ -25,7 +66,8 @@ export const SubscribeForm = () => {
                   text-black"
               placeholder="Name"
               type="text"
-              name="name"
+              name={formName}
+              onChange={(event) => handleInputUpdate(event, formName)}
               required
             />
             <p className="text-left ml-4">
@@ -38,7 +80,8 @@ export const SubscribeForm = () => {
                   text-black"
               placeholder="Email Address"
               type="email"
-              name="email"
+              name={formEmail}
+              onChange={(event) => handleInputUpdate(event, formEmail)}
               required
             />
             <p className="text-left ml-4">
@@ -50,9 +93,10 @@ export const SubscribeForm = () => {
           <input
             className="mt-5 w-auto min-w-[435px] rounded-lg p-3 outline-none
             text-black"
-            name="stockTicker"
+            name={formTicker}
             type="text"
             placeholder="Enter stock ticker (APPL, NVDA, etc.)"
+            onChange={(event) => handleInputUpdate(event, formTicker)}
             required
           />
           <p className="text-left ml-12">
@@ -84,13 +128,14 @@ export const SubscribeForm = () => {
         <div className="flex justify-center -ml-9 mt-5">
           <div className="text-5xl mr-4">$</div>
           <input
-            className="-ml-1 mt-1.5 w-auto min-w-[100px] rounded-lg p-3 outline-none
-              text-black"
-            name="stockTicker"
+            className="-ml-1 mt-1.5 w-auto min-w-[100px] rounded-lg p-3 
+            outline-none text-black"
+            name={formTargetPrice}
             type="number"
             min={0}
             placeholder="Enter target price"
             required
+            onChange={(event) => handleInputUpdate(event, formTargetPrice)}
           />
         </div>
         <p className="mr-24">
@@ -102,11 +147,12 @@ export const SubscribeForm = () => {
             type="submit"
             buttonText="Submit"
             className="SW-Button mx-4 min-w-[150px]"
+            onClick={handleButtonClick}
           />
         </div>
       </form>
     </>
   );
-};
+}
 
 export default SubscribeForm;
