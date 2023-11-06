1afb015255f6625cb752dd33f844dbec49b44ce8
sharonoba <56489515+sharonoba@users.noreply.github.com>
2023-11-05 21:03:07 -0800

    added unit test file and spaces removed(user)

--- database.py
+++ database.py
@@ -7,51 +7,74 @@ import os
 import re
 import json
 
-__BASH_PATH = os.path.expanduser('~/.bash_profile')
-__STOCK_JSON = "./database/stocks.json"  # Currently holds stock data
-__STOCK_CSV = "./database/stocks.csv"
-__USER_FILE = "./database/users.csv"  # Currently holds user data
+class Database:
 
-def extract_api_key(file_path):
-    with open(file_path, 'r') as file:
-        content = file.read()
-        match = re.search(r'^export\s+POLYGON_API_KEY=(.*)$', content, re.MULTILINE)
-        if match:
-            api_key = match.group(1)
+    def __init__(self):
+        self.__USER_FILE = "./database/users.csv"  # Currently holds user data
+        self.__BASH_PATH = os.path.expanduser('~/.bash_profile')
+        self.__STOCK_CSV = "./database/stocks.csv"
+        self.__STOCK_JSON = "./database/stocks.json"  # Currently holds stock 
+        self.__API_KEY = ""
+        self.df_users = pd.read_csv(self.__USER_FILE, delimiter=',')  
+        self.df_stocks = {}     
+
+    def extract_api_key(self):
+        with open(self.__BASH_PATH, 'r') as file:
+            content = file.read()
+            match = re.search(r'^export\s+POLYGON_API_KEY=(.*)$', content, re.MULTILINE)
+            if match:
+                api_key = match.group(1)
+            else:
+                raise ValueError("POLYGON_API_KEY not found in .bash_profile or .bashrc")
+        
+        os.environ['POLYGON_API_KEY']=api_key
+        polygon_api_key = os.environ.get('POLYGON_API_KEY')
+        polygon_api_key = polygon_api_key.replace('"', '')
+        self.__API_KEY = polygon_api_key
+        return polygon_api_key
+
+    def getStockDaily(self):
+        
+        if self.__API_KEY is None:
+            raise EnvironmentError("Polygon API key not found.  Set the environment variable")
+
         else:
-            raise ValueError("POLYGON_API_KEY not found in .bash_profile or .bashrc")
-    
-    os.environ['POLYGON_API_KEY']=api_key
-    polygon_api_key = os.environ.get('POLYGON_API_KEY')
-    polygon_api_key = polygon_api_key.replace('"', '')
-    return polygon_api_key
+            url = f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09?adjusted=true&apiKey={self.__API_KEY}"
+            # Make a GET request to the API and fetch the data
+            response = requests.get(url)
 
-def getStockDaily():
-    
-    if __API_KEY is None:
-        raise EnvironmentError("Polygon API key not found.  Set the environment variable")
+        if response.status_code == 200:
+            data = response.json()
+            self.df_stocks = pd.DataFrame(data['results'])
+            self.df_stocks.to_csv(self.__STOCK_CSV, index=False)
+            with open(self.__STOCK_JSON, 'w') as json_file:
+                json.dump(data, json_file, indent=4)
+                print(f"Data saved successfully to {self.__STOCK_JSON}")
+        else:
+            print(f"Request failed with status code {response.status_code}")
 
-    else:
-        url = f"https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09?adjusted=true&apiKey={__API_KEY}"
-        # Make a GET request to the API and fetch the data
-        response = requests.get(url)
+    def getUserAction(self, row):
+        self.df_users = pd.read_csv(self.__USER_FILE, delimiter=',')
+        return self.df_users['email'][row], self.df_users['stock'][row], self.df_users['price'][row], self.df_users['action'][row]
+        
+    def stockCompare(self, user_info, stock_info):
+        
+        if user_info > stock_info:
+            return True
+        else: 
+            return False
+        
 
-    if response.status_code == 200:
-        data = response.json()
-        df = pd.DataFrame(data['results'])
-        df.to_csv(__STOCK_CSV, index=False)
-        with open(__STOCK_JSON, 'w') as json_file:
-            json.dump(data, json_file, indent=4)
-            print(f"Data saved successfully to {__STOCK_JSON}")
-    else:
-        print(f"Request failed with status code {response.status_code}")
+        
 
-def getAllUsers():
-    user = pd.read_csv(__USER_FILE, delimiter=',')
 
-    print(user)
+whisper01 = Database()
+whisper01.extract_api_key()
+email, stock, price, action = whisper01.getUserAction(1)
+print(email, stock, price, action)
 
-if __name__ == "__main__":
-    __API_KEY = extract_api_key(__BASH_PATH)
-    getStockDaily()
-    getAllUsers()
+    # getStockDaily()
+    # getAllUsers()
+
+
+
---
+++ database_test.py
@@ -0,0 +1,26 @@
+from database import Database
+import unittest
+
+class TestStats(unittest.TestCase):
+    
+
+    def test_should_give_user_info(self):
+        email = "aharris@pdx.edu"
+        stock = "GOOG"
+        price = 150
+        action = "sell"
+        self.assertEqual((email, stock, price, action), Database().getUserAction(0))
+
+    
+
+    def test_should_return_sell(self):
+        user_info = 300
+        stock_info =156
+        self.assertEqual(True, Database().stockCompare(user_info, stock_info))
+    
+    
+
+        
+        
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
