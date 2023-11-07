c31a28b66309ac47175b07a88a88383bfd945ce9
NAlexH2 <alex@nah-red.net>
2023-11-07 14:25:37 -0800

    adding functions to check vars

--- src/util/util.ts
+++ src/util/util.ts
@@ -15,3 +15,21 @@ export function buttonStyleSelector(buyOrSell: string, toChange: string) {
 export function validTicker(tickerToCheck: string): boolean {
   return AllTickers.includes(tickerToCheck);
 }
+
+export function isEmail(email: string): boolean {
+  const regexp = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
+  return regexp.test(email);
+}
+
+export function targetPriceIsNumber(price: string): boolean {
+  try {
+    parseInt(price, 10);
+  } catch (error) {
+    return false;
+  }
+  return true && parseInt(price, 10) >= 0;
+}
+
+export function isBuyOrSell(buyOrSell: string): boolean {
+  return buyOrSell === "Buy" || buyOrSell === "Sell";
+}
